ada47fac5c1a7c25ea33a8bc71ff8d7528c437d9
Lakshan Madushanka <epmadushanka@gmail.com>
2021-08-08 22:02:52 +0530

    Controller report logics moved to a trait

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Services\GenerateReportService;
+use App\Traits\ReportGenerateTraits\UserReportGenerateTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Jimmyjs\ReportGenerator\Facades\PdfReportFacade;
@@ -16,11 +17,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    private $generateReport;
+    use UserReportGenerateTrait;
+
+    private $makeReport;
 
     public function __construct(GenerateReportService $generateReport)
     {
-        $this->generateReport = $generateReport;
+        $this->makeReport = $generateReport;
     }
 
     /**
@@ -49,11 +52,14 @@ class UserController extends Controller
      *
      * @param  int  $id
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show(User $user)
     {
-        return response()->json(['Data' => $user], Response::HTTP_OK);
+        return response()->json(
+            ['Data' => $user],
+            Response::HTTP_OK
+        );
     }
 
     /**
@@ -83,78 +89,12 @@ class UserController extends Controller
 
     public function allUsersReport(Request $request)
     {
-        $orderBy = 'id';
-        $sortBy = 'desc';
-        $groupBy = '';
-
-        //set sort by and maa data
-        if (strtolower($request->query('sorBy') === 'asc')) {
-            $sortBy = 'asc';
-        }
-
-        $orderByQuery = strtolower($request->query('orderBy'));
-        if ($orderByQuery) {
-            $orderBy = $orderByQuery;
-        }
-
-        $groupByQuery = strtolower($request->query('groupBy'));
-        if ($orderByQuery) {
-            $groupBy = $groupByQuery;
-        }
-
-        $queryBuilder = User::select('n_i_c', 'user_id', 'full_name', 'gender' )
-            ->orderBy($groupBy ? $groupBy : $orderBy, $sortBy);
-
-        $columns = [
-            'N I C' => 'n_i_c',
-            'User Id' => 'user_id',
-            'Full Name' => 'full_name',
-            'Gender' => 'gender',
-        ];
-
-        return $this->generateReport
-            ->setMetaData($queryBuilder, $request, $columns, 'allusers', $sortBy, $groupBy)
-            ->generateReport()->editColumn('Gender', ['class' => 'blue'])
-            ->setCss([
-                '.blue' => 'color:blue',
-            ])
-            ->download('AllUsers'.'_'.now());
+        return $this->userAllReport($request);
     }
 
     public function allUsersWithRoles(Request $request)
     {
-
-        $sortBy = 'desc';
-        //set sort by and maa data
-        if (strtolower($request->query('sorBy') === 'asc')) {
-            $sortBy = 'asc';
-        }
-
-        $queryBuilder = DB::table('users')
-            ->leftJoin('user_role', 'users.id', '=', 'user_role.user_id')
-            ->leftJoin('roles', 'roles.id', '=', 'user_role.role_id')
-            ->select('n_i_c', 'users.user_id', 'full_name', 'gender',
-                'roles.id as id', 'roles.name as roleName')
-            ->orderBy('id', $sortBy);
-
-        $columns = [
-            'N I C' => 'n_i_c',
-            'User Id' => 'user_id',
-            'Full Name' => 'full_name',
-            'Gender' => 'gender',
-            'Role' => 'roleName',
-            'Role Id' => 'id',
-        ];
-
-        return $this->generateReport
-            ->setMetaData($queryBuilder, $request, $columns, 'allusers', $sortBy, 'Role Id')
-            ->generateReport()
-            ->editColumn('Role', ['class' => 'bold'])
-            ->editColumn('Role Id', ['class' => 'visible'])
-            ->setCss([
-                '.visible' => 'display:none',
-            ])
-            ->download('AllUsersWithRoles'.'_'.now());
+        return $this->usersReportWithRoles($request);
     }
 
 }
---
+++ app/Traits/ReportGenerateTraits/UserReportGenerateTrait.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Traits\ReportGenerateTraits;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+trait UserReportGenerateTrait
+{
+
+    public function userAllReport(Request $request)
+    {
+        $orderBy = 'id';
+        $sortBy = 'desc';
+        $groupBy = '';
+
+        //set sort by and maa data
+        if (strtolower($request->query('sorBy') === 'asc')) {
+            $sortBy = 'asc';
+        }
+
+        $orderByQuery = strtolower($request->query('orderBy'));
+        if ($orderByQuery) {
+            $orderBy = $orderByQuery;
+        }
+
+        $groupByQuery = strtolower($request->query('groupBy'));
+        if ($orderByQuery) {
+            $groupBy = $groupByQuery;
+        }
+
+        $queryBuilder = User::select('n_i_c', 'user_id', 'full_name', 'gender')
+            ->orderBy($groupBy ? $groupBy : $orderBy, $sortBy);
+
+        $columns = [
+            'N I C' => 'n_i_c',
+            'User Id' => 'user_id',
+            'Full Name' => 'full_name',
+            'Gender' => 'gender',
+        ];
+
+        return $this->makeReport
+            ->setMetaData($queryBuilder, $request, $columns, 'allusers', $sortBy, $groupBy)
+            ->generateReport()
+            ->editColumn('Gender', ['class' => 'blue'])
+            ->setCss([
+                '.blue' => 'color:blue',
+            ])
+            ->download('AllUsers'.'_'.now());
+    }
+
+
+    public function usersReportWithRoles(Request $request)
+    {
+        $sortBy = 'desc';
+        //set sort by and maa data
+        if (strtolower($request->query('sorBy') === 'asc')) {
+            $sortBy = 'asc';
+        }
+
+        $queryBuilder = DB::table('users')
+            ->leftJoin('user_role', 'users.id', '=', 'user_role.user_id')
+            ->leftJoin('roles', 'roles.id', '=', 'user_role.role_id')
+            ->select('n_i_c', 'users.user_id', 'full_name', 'gender',
+                'roles.id as id', 'roles.name as roleName')
+            ->orderBy('id', $sortBy);
+
+        $columns = [
+            'N I C' => 'n_i_c',
+            'User Id' => 'user_id',
+            'Full Name' => 'full_name',
+            'Gender' => 'gender',
+            'Role' => 'roleName',
+            'Role Id' => 'id',
+        ];
+
+        return $this->makeReport
+            ->setMetaData($queryBuilder, $request, $columns, 'allusers', $sortBy, 'Role Id')
+            ->generateReport()
+            ->editColumn('Role', ['class' => 'bold'])
+            ->editColumn('Role Id', ['class' => 'visible'])
+            ->setCss([
+                '.visible' => 'display:none',
+            ])
+            ->download('AllUsersWithRoles'.'_'.now());
+    }
+
+}
